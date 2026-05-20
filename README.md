d7ae6360df52da0ed477abe7093b3a708c80b6f9
Bendeguz <csermendy.bendeguz@icloud.com>
2026-05-20 16:11:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Library
+Library description goes here.
---
+++ src/Library.cpp
@@ -0,0 +1,20 @@
+/*
+  Library.cpp - //short description\\.
+  Created by Csermendy Bendeguz, //MONTH DAY, YEAR\\.
+  //LICENSE\\
+*/
+
+#include "Arduino.h"
+#include "Library.h"
+
+library::library() {
+  
+}
+
+void library::begin() {
+  
+}
+
+void library::function1() {
+  
+}
